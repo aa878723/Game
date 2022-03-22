60b2b2b284f7e5558435a655f396c0073bf47004
aa878723 <ccc26841330@gmail.com>
2022-03-22 16:50:12 +0800

    新增儲值斯聊表格

---
+++ Game/Game/Models/Money.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Game.Models
+{
+    public partial class Money
+    {
+        public int Id { get; set; }
+        public string Account { get; set; } = null!;
+        public string Card { get; set; } = null!;
+        public decimal Qe { get; set; }
+    }
+}
---
+++ Game/Game/Models/PersonalTalking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Game.Models
+{
+    public partial class PersonalTalking
+    {
+        public int Id { get; set; }
+        public string Account { get; set; } = null!;
+        public string Picture { get; set; } = null!;
+        public string Message { get; set; } = null!;
+        public DateTime SendTime { get; set; }
+        public string Friend { get; set; } = null!;
+    }
+}
